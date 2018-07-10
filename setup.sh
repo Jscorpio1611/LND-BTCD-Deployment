cca60c86dd34963f1393bc1b92450defd06a573d
Peter Ullrich <PJUllrich@users.noreply.github.com>
2018-07-10 17:37:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git clone https://github.com/lightningnetwork/lnd.git tmp
-mv tmp/docker/ltcd tmp/docker/lnd tmp/docker/btcd -t . 
+mv tmp/docker/ltcd tmp/docker/lnd tmp/docker/btcd tmp/docker/docker-compose.yml . 
 rm -rf tmp 
